9b24030591ed271bbc965b1539ac22a62be6fe5f
Vheos <Vheos777@gmail.com>
2023-09-23 00:12:02 +0200

    Moved `CubeTogglePrefab` to `UISettings`

--- Assets/Code/Runtime/CubeToggleGroup.cs
+++ Assets/Code/Runtime/CubeToggleGroup.cs
@@ -9,36 +9,10 @@ using UnityEngine.UI;
 [RequireComponent(typeof(ToggleGroup))]
 public class CubeToggleGroup : MonoBehaviour
 {
-	[SerializeField] CubeToggle cubeTogglePrefab;
 	private List<CubeToggle> toggles;
-	public event Action<CubeToggle, CubeToggle> OnToggleChanged;
 	public CubeToggle ActiveToggle { get; private set; }
+	public event Action<CubeToggle, CubeToggle> OnToggleChanged;
 
-
-	private void Awake()
-	{
-		// Destroy placeholder toggles
-		for (int i = transform.childCount - 1; i >= 0; i--)
-			DestroyImmediate(transform.GetChild(i).gameObject);
-
-		toggles = new List<CubeToggle>();
-		ToggleGroup toggleGroup = GetComponent<ToggleGroup>();
-		int[] amounts = Settings.UI.CubeToggleAmounts;
-		Color[] colors = Settings.UI.CubeToggleIconColors;
-
-		// Instantiate and initialize new toggles
-		for (int i = 0; i < amounts.Length; i++)
-		{
-			var newToggle = Instantiate(cubeTogglePrefab, transform);
-			newToggle.Amount = amounts[i];
-			newToggle.ToggleGroup = toggleGroup;
-			if (i < colors.Length)
-				newToggle.CubeColor = colors[i];
-
-			newToggle.AddCallbackOnStateChanged(state => InvokeOnToggleChanged(newToggle, state));
-			toggles.Add(newToggle);
-		}
-	}
 	private void InvokeOnToggleChanged(CubeToggle toggle, bool state)
 	{
 		CubeToggle previousActiveToggle = ActiveToggle;
@@ -47,5 +21,32 @@ public class CubeToggleGroup : MonoBehaviour
 			return;
 
 		OnToggleChanged?.Invoke(previousActiveToggle, ActiveToggle);
+	}
+	private void Awake()
+	{
+		ToggleGroup toggleGroup = GetComponent<ToggleGroup>();
+		toggles = new List<CubeToggle>();
+
+		// Destroy placeholder toggles
+		for (int i = transform.childCount - 1; i >= 0; i--)
+			DestroyImmediate(transform.GetChild(i).gameObject);
+
+		// Cache settings
+		int[] amounts = Settings.UI.CubeToggleAmounts;
+		Color[] colors = Settings.UI.CubeToggleIconColors;
+		CubeToggle prefab = Settings.UI.CubeTogglePrefab;
+
+		// Instantiate and initialize new toggles
+		for (int i = 0; i < amounts.Length; i++)
+		{
+			var newToggle = Instantiate(prefab, transform);
+			newToggle.Amount = amounts[i];
+			newToggle.ToggleGroup = toggleGroup;
+			if (i < colors.Length)
+				newToggle.CubeColor = colors[i];
+
+			newToggle.AddCallbackOnStateChanged(state => InvokeOnToggleChanged(newToggle, state));
+			toggles.Add(newToggle);
+		}
 	}
 }
