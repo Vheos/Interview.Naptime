4ed7aa44ef0d51b2c28073fed2654e1ad648f168
Vheos <Vheos777@gmail.com>
2023-09-23 00:12:42 +0200

    Added component `RootCanvas`

---
+++ Assets/Code/Runtime/RootCanvas.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+public class RootCanvas : MonoBehaviour
+{
+	[SerializeField] private Canvas startMenuCanvas;
+	[SerializeField] private Canvas gameOverCanvas;
+	private Canvas canvas;
+
+	public bool IsVisible
+	{
+		get => gameObject.activeSelf;
+		set => gameObject.SetActive(value);
+	}
+	private IEnumerable<Canvas> AllCanvases
+	{
+		get
+		{
+			yield return startMenuCanvas;
+			yield return gameOverCanvas;
+		}
+	}
+
+	public void ChangeScreen(UIScreen screen)
+	{
+		var visibleCanvas = screen switch
+		{
+			UIScreen.StartMenu => startMenuCanvas,
+			UIScreen.GameOver => gameOverCanvas,
+			_ => null,
+		};
+
+		if(visibleCanvas == null)
+		{
+			IsVisible = false;
+			return;
+		}
+
+		IsVisible = true;
+		foreach (var subCanvas in AllCanvases)
+			subCanvas.gameObject.SetActive(subCanvas == visibleCanvas);
+	}
+
+	private void Awake()
+	{
+		canvas = GetComponent<Canvas>();
+	}
+}
