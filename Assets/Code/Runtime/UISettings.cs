83145d0db4ede2f0c86db1c1c7ef0797f2479f98
Vheos <Vheos777@gmail.com>
2023-09-22 20:51:11 +0200

    Updated `UISettings`

--- Assets/Code/Runtime/UISettings.cs
+++ Assets/Code/Runtime/UISettings.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = nameof(UISettings), menuName = "Settings/" + nameof(UISettings))]
 public class UISettings : ScriptableObject
 {
-	[SerializeField] private Color fontShadow;
-	[SerializeField] private Color mainBackground;
-	[SerializeField] private Color gameOverBar;
+	[field: SerializeField] public Color FontShadow { get; private set; }
+	[field: SerializeField] public Color MainBackground { get; private set; }
+	[field: SerializeField] public Color GameOverBar { get; private set; }
+	[field: SerializeField, Range(2, 500)] public int[] CubeToggleAmounts { get; private set; }
+	[field: SerializeField] public Color[] CubeToggleIconColors { get; private set; }
 }
