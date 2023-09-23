854e546790c2471f0b9e9199fa123a7394486d59
Vheos <Vheos777@gmail.com>
2023-09-23 14:44:46 +0200

    Added gameplay logic for `Shooter` and `Bullet`

--- Assets/Code/Runtime/Gameplay/Bullet.cs
+++ Assets/Code/Runtime/Gameplay/Bullet.cs
@@ -4,4 +4,50 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+	private Vector3 direction;
+	private float despawnTime;
+
+	public void Initialize(Vector3 position, Vector3 direction)
+	{
+		transform.position = position;
+		this.direction = direction.normalized;
+		despawnTime = Time.time + Settings.Game.BulletLifetime;
+	}
+	private void Move()
+		=> transform.position += Time.deltaTime * Settings.Game.BulletSpeed * direction;
+	private bool CheckDespawn()
+	{
+		if (Time.time < despawnTime)
+			return false;
+
+		GameManager.Despawn(this);
+		return true;
+	}
+
+	private bool CheckHit()
+	{
+		if (!Physics.CheckSphere(transform.position, Settings.Game.BulletRadius, GameManager.ShooterLayerMask))
+			return false;
+
+		Collider[] colliders = new Collider[1];
+		Physics.OverlapSphereNonAlloc(transform.position, Settings.Game.BulletRadius, colliders, GameManager.ShooterLayerMask);
+		Shooter hitShooter = colliders[0].GetComponent<Shooter>();
+		hitShooter.Health--;
+
+		GameManager.Despawn(this);
+		return true;
+	}
+
+	private void Update()
+	{
+		if (CheckDespawn())
+			return;
+
+		Move();
+	}
+
+	private void FixedUpdate()
+	{
+		CheckHit();
+	}
 }
--- Assets/Code/Runtime/Gameplay/Shooter.cs
+++ Assets/Code/Runtime/Gameplay/Shooter.cs
@@ -1,7 +1,96 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Shooter : MonoBehaviour
 {
+	[SerializeField] Transform bulletSpawnPoint;
+
+	private int health;
+	public int Health
+	{
+		get => health;
+		set
+		{
+			int previousHealth = health;
+			health = value;
+			if (health == previousHealth)
+				return;
+
+			OnHealthChanged?.Invoke(previousHealth, health);
+
+			if (health < previousHealth)
+				GameManager.StartCoroutine(TakeDamageCoroutine());
+		}
+	}
+
+	private float nextRotateTime;
+	private float nextShootTime;
+
+	public event Action<int, int> OnHealthChanged;
+	public event Action OnDeath;
+
+	private void InitializeHealth()
+		=> health = Settings.Game.ShooterHealth;
+	private void UpdateNextRotateTime()
+	{
+		Vector2 intervalRange = Settings.Game.ShooterRotateInterval;
+		float interval = Random.Range(intervalRange.x, intervalRange.y);
+		nextRotateTime = Time.time + interval;
+	}
+	private void UpdateNextShootTime()
+		=> nextShootTime = Time.time + Settings.Game.ShooterShootInterval;
+
+	private void CheckRotate()
+	{
+		if (Time.time < nextRotateTime)
+			return;
+
+		Vector2 angleRange = Settings.Game.ShooterRotateAngle;
+		float angle = Random.Range(angleRange.x, angleRange.y);
+		transform.Rotate(Vector3.forward, angle, Space.World);
+		UpdateNextRotateTime();
+	}
+	private void CheckShoot()
+	{
+		if (Time.time < nextShootTime)
+			return;
+
+		Bullet newBullet = GameManager.SpawnBullet();
+		newBullet.Initialize(bulletSpawnPoint.position, transform.forward);
+		UpdateNextShootTime();
+	}
+
+	private IEnumerator TakeDamageCoroutine()
+	{
+		if (Health <= 0)
+		{
+			GameManager.Despawn(this);
+			OnDeath?.Invoke();
+			yield break;
+		}
+
+		gameObject.SetActive(false);
+		yield return new WaitForSeconds(Settings.Game.ShooterRespawnInterval);
+		if (this != null)
+			gameObject.SetActive(true);
+
+	}
+
+	private void Awake()
+	{
+		InitializeHealth();
+	}
+	private void OnEnable()
+	{
+		UpdateNextRotateTime();
+		UpdateNextShootTime();
+	}
+	private void Update()
+	{
+		CheckRotate();
+		CheckShoot();
+	}
 }
