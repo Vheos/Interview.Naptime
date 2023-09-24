b2cb0260143cfe181472719f273597d81c1931ba
Vheos <Vheos777@gmail.com>
2023-09-24 20:03:44 +0200

    Added setting `UseTweens`

--- Assets/Code/Runtime/Settings/GameSettings.cs
+++ Assets/Code/Runtime/Settings/GameSettings.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = nameof(GameSettings), menuName = "Settings/" + nameof(GameSettings))]
 public class GameSettings : ScriptableObject
-{
+{ 
+	[field: Header("General")]
+	[field: SerializeField] public bool UseTweens { get; private set; }
+
 	[field: Header("Shooter")]
 	[field: SerializeField] public Shooter ShooterPrefab { get; private set; }
 	[field: SerializeField, Range(1, 5)] public int ShooterHealth { get; private set; }
@@ -11,7 +14,6 @@ public class GameSettings : ScriptableObject
 	[field: SerializeField, Range(0.5f, 2f)] public float ShooterShootInterval { get; private set; }
 	[field: SerializeField, Range(0f, 5f)] public float ShooterRespawnInterval { get; private set; }
 	[field: SerializeField, Range(0f, 1f)] public float ShooterSpawnInterval { get; private set; }
-
 
 	[field: Header("Bullet")]
 	[field: SerializeField] public Bullet BulletPrefab { get; private set; }
